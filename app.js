f486dd744149a8b353187074cfaf26ac1355fb98
JenSarela <jen.sarela24@gmail.com>
2019-03-27 22:55:05 -0300

    subiendo ejercicios de conversion

---
+++ app.js
@@ -0,0 +1,6 @@
+//Preguntar la tempreatura en Celcius
+var temperature = parseInt(prompt("¿Cuál es la temperatura en Celsius?"));
+//Convertir Celsius a Farenheit
+var convertToFarenheith = (temperature*1.8) + 32;
+//Mostrar resultados en la consola
+console.log("La temperatura en Farenheit es " + convertToFarenheith);
